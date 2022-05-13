6ab3d8bab23496f33459c612fba3d3ed84c9e710
Fergal M <47755843+Gretsok@users.noreply.github.com>
2022-05-13 12:01:00 +0200

    Player Custom Physics

--- Assets/Gameplay/PlayerController/PlayerMovementController.cs
+++ Assets/Gameplay/PlayerController/PlayerMovementController.cs
@@ -9,12 +9,12 @@ namespace RLS.Gameplay.PlayerController
         private CharacterController m_characterController;
         [SerializeField]
         private Transform m_cameraTarget;
+        [SerializeField]
+        private Transform m_topHead = null;
 
-        [Header("Params")]
+        [Header("Gameplay Params")]
         [SerializeField]
         private float m_speed = 5f;
-        [SerializeField]
-        private Vector2 m_cameraSensitivity = new Vector2(60f, 60f);
         [SerializeField]
         private float m_jumpCooldown = 1f;
         private float m_lastJumpTime = 0f;
@@ -27,9 +27,17 @@ namespace RLS.Gameplay.PlayerController
         private float m_gravity = 9.81f;
         [SerializeField]
         private float m_jumpSpeed = 6f;
+
         private bool m_isGrounded = false;
+        private bool m_isHeadTouchingCeiling = false;
 
         private float m_verticalVelocity = 0f;
+
+        [Header("Camera Settings")]
+        [SerializeField]
+        private Vector2 m_cameraSensitivity = new Vector2(60f, 60f);
+        [SerializeField]
+        private Vector2 m_cameraClamping = new Vector2(-60, 60);
 
         private void Start()
         {
@@ -71,6 +79,14 @@ namespace RLS.Gameplay.PlayerController
 
         private void apply_movement()
         {
+            if(m_isHeadTouchingCeiling)
+            {
+                if(m_verticalVelocity > 0f)
+                {
+                    m_verticalVelocity = 0f;
+                }
+                
+            }
             if(m_isGrounded && m_verticalVelocity < 0f)
             {
                 m_verticalVelocity = -1f;
@@ -94,14 +110,34 @@ namespace RLS.Gameplay.PlayerController
         {
             transform.Rotate(Vector3.up * m_lookAroundValues.x * m_cameraSensitivity.x * Time.deltaTime);
             m_cameraTarget.Rotate(Vector3.right * -m_lookAroundValues.y * m_cameraSensitivity.y * Time.deltaTime);
+
+            if(m_cameraTarget.localEulerAngles.x < 360f + m_cameraClamping.x && m_cameraTarget.localEulerAngles.x > 180f)
+            {
+                var eulerTemp = m_cameraTarget.localEulerAngles;
+                eulerTemp.x = 360f + m_cameraClamping.x;
+                m_cameraTarget.localEulerAngles = eulerTemp;
+            }
+
+            else if (m_cameraTarget.localEulerAngles.x > m_cameraClamping.y && m_cameraTarget.localEulerAngles.x < 180f)
+            {
+                var eulerTemp = m_cameraTarget.localEulerAngles;
+                eulerTemp.x = m_cameraClamping.y;
+                m_cameraTarget.localEulerAngles = eulerTemp;
+            }
         }
 
         private void Update()
         {
+            
             check_if_grounded();
             apply_rotation();
             apply_movement();
             
+        }
+
+        private void FixedUpdate()
+        {
+            check_if_touched_ceiling();
         }
 
         #region Specific Generic Actions
@@ -109,6 +145,26 @@ namespace RLS.Gameplay.PlayerController
         {
             m_verticalVelocity = m_jumpSpeed;
         }
+        #endregion
+
+        #region Environment Checking
+
+        private void check_if_touched_ceiling()
+        {
+            m_isHeadTouchingCeiling = false;
+            if (Physics.Raycast(m_topHead.position + Vector3.down * 0.5f, Vector3.up, 0.6f))
+            {
+                m_isHeadTouchingCeiling = true;
+                //Debug.DrawLine(m_topHead.position + Vector3.down * 0.5f, m_topHead.position + Vector3.down * 0.5f + Vector3.up * 0.6f, Color.red);
+            }
+            /*else
+            {
+                Debug.DrawLine(m_topHead.position + Vector3.down * 0.5f, m_topHead.position + Vector3.down * 0.5f + Vector3.up * 0.6f, Color.green);
+
+            }*/
+        }
+
+
         #endregion
     }
 }
