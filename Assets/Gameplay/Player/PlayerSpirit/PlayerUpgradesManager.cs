86a318a565defc8e010ddcf119a23a58644b241b
Yann Ray <51749201+artifroid42@users.noreply.github.com>
2022-06-03 13:12:40 +0200

    Upgrades Bugfix

--- Assets/Gameplay/Player/PlayerSpirit/PlayerUpgradesManager.cs
+++ Assets/Gameplay/Player/PlayerSpirit/PlayerUpgradesManager.cs
@@ -107,9 +107,9 @@ namespace RLS.Gameplay.Player
                     }
                     else
                     {
-                        if (m_currentStatsData != null && CheckUpgrade(m_currentStatsData.Class, (EUpgrade)(a_inputNumber + 1)))
+                        if (m_currentStatsData != null && CheckUpgrade(m_currentStatsData.Class, (EUpgrade)(a_inputNumber - 1)))
                         {
-                            IncreaseLevelUpgrade(m_currentStatsData.Class, (EUpgrade)(a_inputNumber + 1));
+                            IncreaseLevelUpgrade(m_currentStatsData.Class, (EUpgrade)(a_inputNumber - 1));
                         }
                     }     
                     
